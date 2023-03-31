9849bd9867de3af241f6ea0440ddabef577cf1c6
Lil-Adonis <adenugaib24@gmail.com>
2023-03-31 05:56:36 -1100

    :chart_with_upwards_trend: New Footer Added
    Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,41 +5,59 @@ type FooterProps = {};
 
 const Footer: React.FC<FooterProps> = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
-      <div className="space-y-4 text-xs text-gray-800"
-      >
-        <h5 className="font-bold">ABOUT</h5>
-        <p>Newsroom</p>
-        <p>Learn about new features</p>
-        <p>Letter from our founders</p>
-        <p>Careers</p>
-        <p>Investors</p>
-      </div>
-      <div className="space-y-4 text-xs text-gray-800"
-      >
-        <h5 className="font-bold">Support</h5>
-        <p>Help Center</p>
-        <p>AirCover</p>
-        <p>Cancellation options</p>
-        <p>Safety information</p>
-        <p>Report a neighborhood concern</p>
-      </div>
-      <div className="space-y-4 text-xs text-gray-800"
-      >
-        <h5 className="font-bold">Community</h5>
-        <p>Airbnb.org: disaster relief housing</p>
-        <p>Support Afghan refugees</p>
-        <p>Combating discrimination</p>
-      </div>
-      <div className="space-y-4 text-xs text-gray-800"
-      >
-        <h5 className="font-bold">Hosting</h5>
-        <p>Try hosting</p>
-        <p>AirCover for Hosts</p>
-        <p>Explore hosting resources</p>
-        <p>How to host responsibly</p>
-      </div>
+    <div className="bg-gray-900 text-gray-300">
+    <div className="max-w-screen-xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
+    <div className="flex flex-col lg:flex-row justify-between flex-wrap">
+    <div className="w-full lg:w-2/5 mb-8 lg:mb-0">
+    <img src="https://www.airbnb.com.br/static/images/logo/logo-225x225.png" alt="Airbnb logo" className="h-12 mb-4" />
+    <p className="text-sm">© Airbnb, Inc. Todos os direitos reservados</p>
     </div>
-  );
-};
-export default Footer;
+    <div className="w-full lg:w-1/5 mb-8 lg:mb-0">
+    <h4 className="text-lg mb-4 font-medium">Sobre</h4>
+    <ul className="text-sm">
+    <li className="mb-2"><a href="#">Como funciona o Airbnb</a></li>
+    <li className="mb-2"><a href="#">Sala de imprensa</a></li>
+    <li className="mb-2"><a href="#">Airbnb Plus</a></li>
+    <li className="mb-2"><a href="#">Airbnb Luxe</a></li>
+    <li className="mb-2"><a href="#">HotelTonight</a></li>
+    <li><a href="#">Airbnb for Work</a></li>
+    </ul>
+    </div>
+    <div className="w-full lg:w-1/5 mb-8 lg:mb-0">
+    <h4 className="text-lg mb-4 font-medium">Comunidade</h4>
+    <ul className="text-sm">
+    <li className="mb-2"><a href="#">Diversidade e pertencimento</a></li>
+    <li className="mb-2"><a href="#">Acessibilidade</a></li>
+    <li className="mb-2"><a href="#">Convidar amigos</a></li>
+    <li><a href="#">Airbnb Associates</a></li>
+    </ul>
+    </div>
+    <div className="w-full lg:w-1/5 mb-8 lg:mb-0">
+    <h4 className="text-lg mb-4 font-medium">Anfitrião</h4>
+    <ul className="text-sm">
+    <li className="mb-2"><a href="#">Hospedar no Airbnb</a></li>
+    <li className="mb-2"><a href="#">Hospedagem responsável</a></li>
+    <li><a href="#">Airbnb.org</a></li>
+    </ul>
+    </div>
+    </div>
+    <div className="flex justify-between flex-wrap pt-8">
+    <div className="text-sm">
+    <a href="#">Política de Privacidade</a>
+    <span className="mx-2">·</span>
+    <a href="#">Termos</a>
+    <span className="mx-2">·</span>
+    <a href="#">Mapa do Site</a>
+    <span className="mx-2">·</span>
+    <a href="#">Detalhes da Empresa</a>
+    </div>
+    <div className="text-sm">
+    <p>Feito com ♥ em São Francisco</p>
+    </div>
+    </div>
+    </div>
+    </div>
+    );
+    }
+    
+    export default Footer;
